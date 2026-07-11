66f1fde41c8a882f4e77f821fcc9b4c00c376ef2
root <root@srv1806220.hstgr.cloud>
2026-07-11 13:28:10 +0000

    Add production frontend origins to CORS

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.use(helmet());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "http://localhost:5175",
   "https://hoanightmares.org",
   "https://www.hoanightmares.org",
 ];
