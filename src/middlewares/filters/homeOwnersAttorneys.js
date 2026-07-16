cb359bbf2a56eb7b565d2512bbb9e0c7e3fc3587
Aryandhiman77 <aryandhiman015@gmail.com>
2026-07-16 10:30:42 +0530

    fixed backend filter

--- src/middlewares/filters/homeOwnersAttorneys.js
+++ src/middlewares/filters/homeOwnersAttorneys.js
@@ -1,32 +1,39 @@
 const homeOwnerAttorneysFilters = (req, _, next) => {
-  const { state, city, country, practice_area, keyword } = req.query;
-  let query = { status: "approved", isPublished: true };
+  const { state, city, county, practice_area, keyword } = req.query;
+  let query = { status: "published", isPublished: true };
   if (state) {
-    query.attorney_state = state;
+    query.attorney_state = state.toLowerCase();
   }
   if (city) {
-    query.attorney_city = city;
+    query.attorney_city = city.toLowerCase();
   }
-  if (country) {
-    query.attorney_county = country;
+  if (county) {
+    query.attorney_county = county.toLowerCase();
   }
   if (practice_area) {
     if (practiceArea) {
       query.attorney_practice_areas = {
-        $in: Array.isArray(practiceArea) ? practiceArea : [practiceArea],
+        $in: Array.isArray(practiceArea)
+          ? practiceArea.toLowerCase()
+          : [practiceArea.toLowerCase()],
       };
     }
   }
   if (keyword) {
     query.$or = [
-      { attorney_name: { $regex: keyword, $options: "i" } },
-      { attorney_firm: { $regex: keyword, $options: "i" } },
-      { attorney_summary: { $regex: keyword, $options: "i" } },
-      { attorney_bio: { $regex: keyword, $options: "i" } },
-      { attorney_city: { $regex: keyword, $options: "i" } },
-      { attorney_state: { $regex: keyword, $options: "i" } },
-      { attorney_county: { $regex: keyword, $options: "i" } },
-      { attorney_practice_areas: { $regex: keyword, $options: "i" } },
+      { attorney_name: { $regex: keyword.toLowerCase(), $options: "i" } },
+      { attorney_firm: { $regex: keyword.toLowerCase(), $options: "i" } },
+      { attorney_summary: { $regex: keyword.toLowerCase(), $options: "i" } },
+      { attorney_bio: { $regex: keyword.toLowerCase(), $options: "i" } },
+      { attorney_city: { $regex: keyword.toLowerCase(), $options: "i" } },
+      { attorney_state: { $regex: keyword.toLowerCase(), $options: "i" } },
+      { attorney_county: { $regex: keyword.toLowerCase(), $options: "i" } },
+      {
+        attorney_practice_areas: {
+          $regex: keyword.toLowerCase(),
+          $options: "i",
+        },
+      },
     ];
   }
   req.hoa_query = query;
